df27f9022b1a1f2b9bf175ff454976e7b9898814
mdevos <martadevos@iesnervion.es>
2022-10-20 14:41:11 +0200

    Agregar archivos de proyecto.

---
+++ 07-Actividades-MVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using _07_Actividades_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _07_Actividades_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            String saludo;
+
+            if (DateTime.Now.Hour > 20 && DateTime.Now.Hour < 6)
+            {
+                saludo = "Buenas nochese";
+            }
+            else if (DateTime.Now.Hour >= 6 && DateTime.Now.Hour < 12)
+            {
+                saludo = "Buenos días";
+            }
+            else
+            {
+                saludo = "Buenas tardes";
+            }
+
+            ViewData["Saludo"] = saludo;
+
+
+            String fecha;
+            fecha = DateTime.Now.ToString();
+            ViewBag.Fecha = fecha;
+
+
+            return View();
+        }
+        
+    }
+}
